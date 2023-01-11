a7ea95f3399fab3d0005c8d555963d6a0c2ff6d4
Habibur Rahman <41167924+habibur899@users.noreply.github.com>
2023-01-11 23:16:30 +0600

    page

---
+++ page.php
@@ -0,0 +1,8 @@
+<?php get_header();
+get_template_part( '/template-part/breadcrumb' ); ?>
+<div class="container">
+    <div class="row">
+		<?php the_content(); ?>
+    </div>
+</div>
+<?php get_footer() ?>
